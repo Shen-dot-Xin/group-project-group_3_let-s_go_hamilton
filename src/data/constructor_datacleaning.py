6f9c6d1ef40a7d375ff1b74726fc1cb21e6377db
Shen-dot-Xin <59472523+Shen-dot-Xin@users.noreply.github.com>
2021-03-29 12:19:32 -0400

    Initial Commit: setup

---
+++ src/data/constructor_datacleaning.py
@@ -0,0 +1,6 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC This notebook contains the codes which clean the datasets related to the f1 constructor success prediction.
+
+# COMMAND ----------
+
