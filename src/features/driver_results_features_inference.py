ef385e4dd45d4105b572dee79902006659725339
Pruthvi Reddy <pruthvi5007@gmail.com>
2021-04-01 15:17:22 -0400

    moving feature file

---
+++ src/features/driver_results_features_inference.py
@@ -0,0 +1,25 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC ## driver_results Feature Engineering for Inference Question (Q1)
+# MAGIC 
+# MAGIC This file is for feature engineering and preparation for Inference
+
+# COMMAND ----------
+
+#Loading required Functions and Packages
+from pyspark.sql import Window
+from pyspark.sql.functions import datediff, current_date, avg, col, round, upper, max, min, lit, sum, countDistinct, concat
+import pyspark.sql.functions as psf
+from pyspark.sql.types import IntegerType
+import numpy as np
+import matplotlib as mp
+import pandas as pd
+
+# COMMAND ----------
+
+#Reading the Processed data from Group's S3 bucket
+driver_race_results = pd.read_csv('s3://group3-gr5069/processed/driver_race_results.csv', header=True)
+
+# COMMAND ----------
+
