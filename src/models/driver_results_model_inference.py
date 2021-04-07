31214f9f8c90be9a9472dab315c4636c3f44ffc9
Pruthvi Reddy <pruthvi5007@gmail.com>
2021-04-07 18:45:38 -0400

    inference model

--- src/models/driver_results_model_inference.py
+++ src/models/driver_results_model_inference.py
@@ -1,7 +1,7 @@
 # Databricks notebook source
 # MAGIC %md
 # MAGIC 
-# MAGIC ## driver_results Model for Inference(Q1)
+# MAGIC ## Driver Results Model for Inference(Q1)
 # MAGIC 
 # MAGIC This file is for develping a model to predict the second posiiton in a rae for Inference(Q1)
 
@@ -13,15 +13,11 @@ from pyspark.sql.functions import datediff, current_date, avg, col, round, upper
 import pyspark.sql.functions as psf
 from pyspark.sql.types import IntegerType
 import numpy as np
-import matplotlib as mp
 import pandas as pd
-import numpy as np
 from datetime import datetime
 import seaborn as sns
 import tempfile
-
 import matplotlib.pyplot as plt
-
 from numpy import savetxt
 
 import mlflow.sklearn
@@ -32,79 +28,27 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 
-# COMMAND ----------
-
-
-driver_race_results_model =driver_race_results.select("*").toPandas()
-driver_race_results_model = driver_race_results_model.dropna()
-
-X_train, X_test, y_train, y_test = train_test_split(driver_race_results_model, driver_race_results_model[["positionOrder"]].values.ravel(), random_state=42)
+from pyspark.ml.classification import LogisticRegression
 
 # COMMAND ----------
 
-## Run 1 - Run Params: {n_estimators = 100; max_depth = 6; max_features = 3}
+#Reading Drivers Performance data prepared for modeling with features
+driverRaceDF= spark.read.csv('s3://group3-gr5069/processed/driver_race_results_mod_feat.csv', header=True, inferSchema=True)
+driverRaceDF = driverRaceDF.drop('_c0')
 
-# Enable autolog()
-# mlflow.sklearn.autolog() requires mlflow 1.11.0 or above.
-mlflow.sklearn.autolog()
+# COMMAND ----------
 
-# With autolog() enabled, all model parameters, a model score, and the fitted model are automatically logged.  
-with mlflow.start_run():
-  
-  # Set the model parameters. 
-  n_estimators = 100
-  max_depth = 6
-  max_features = 3
-  
-  # Create and train model.
-  rf = RandomForestRegressor(n_estimators = n_estimators, max_depth = max_depth, max_features = max_features)
-  rf.fit(X_train, y_train)
-  
-  # Use the model to make predictions on the test dataset.
-  predictions = rf.predict(X_test)
-  
-  # Create metrics
-  mse = mean_squared_error(y_test, predictions)
-  mae = mean_absolute_error(y_test, predictions)
-  r2 = r2_score(y_test, predictions)
-  print("  mse: {}".format(mse))
-  print("  mae: {}".format(mae))
-  print("  R2: {}".format(r2))
-  
-  # Log metrics
-  mlflow.log_metric("mse", mse)
-  mlflow.log_metric("mae", mae)  
-  mlflow.log_metric("r2", r2) 
-  
-  # Create feature importance
-  importance = pd.DataFrame(list(zip(driver_race_results_model.columns, rf.feature_importances_)), 
-                              columns=["Feature", "Importance"]
-                            ).sort_values("Importance", ascending=False)
+driverRaceTrainDF = driverRaceDF.filter(driverRaceDF.raceYear <= 2010)
+driverRaceTestDF = driverRaceDF.filter(driverRaceDF.raceYear > 2010)
 
-  # Log importances using a temporary file
-  temp = tempfile.NamedTemporaryFile(prefix="feature-importance-", suffix=".csv")
-  temp_name = temp.name
-  try:
-    importance.to_csv(temp_name, index=False)
-    mlflow.log_artifact(temp_name, "feature-importance.csv")
-  finally:
-    temp.close() # Delete the temp file
+# COMMAND ----------
 
-  # Create plot
-  fig, ax = plt.subplots()
+driverRaceTrainDF= driverRaceTrainDF.sort(driverRaceTrainDF.raceYear.desc())
+driverRaceTrainDF.display()
 
-  sns.residplot(predictions, y_test, lowess=True)
-  plt.xlabel("Predicted values for Price ($)")
-  plt.ylabel("Residual")
-  plt.title("Residual Plot")
+# COMMAND ----------
 
-  # Log residuals using a temporary file
-  temp = tempfile.NamedTemporaryFile(prefix="residuals-", suffix=".png")
-  temp_name = temp.name
-  try:
-    fig.savefig(temp_name)
-    mlflow.log_artifact(temp_name, "residuals.png")
-  finally:
-    temp.close() # Delete the temp file
+driverRaceTrainDF.describe().display()
 
-  display(fig)
+# COMMAND ----------
+
