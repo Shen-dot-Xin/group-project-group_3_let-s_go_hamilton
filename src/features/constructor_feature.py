b255381c9c0017ea9a6c8091956d1f7177e5d1f6
Shen-dot-Xin <59472523+Shen-dot-Xin@users.noreply.github.com>
2021-04-13 13:18:56 -0400

    Add comments, Add feature description

--- src/features/constructor_feature.py
+++ src/features/constructor_feature.py
@@ -7,16 +7,19 @@
 # MAGIC 
 # MAGIC This notebook reads the processed data and engineers the features. 
 # MAGIC 
-# MAGIC  The following features are engineered: 
-# MAGIC  - Circuits
-# MAGIC  - Average Driver Point per race
-# MAGIC  - First Participation
-# MAGIC  - Engine Problems
-# MAGIC  - The number of completed races
-# MAGIC  - Average Fastest Lap
-# MAGIC  - Average Fastest Speed
-# MAGIC  - Number of unique drivers
-# MAGIC  - Constructor ranking
+# MAGIC  The features are engineered by the following criteria: 
+# MAGIC  
+# MAGIC  | Features | Name| Variable Type|Description|
+# MAGIC  | ---------|-----|--------------|-----------|
+# MAGIC  | Circuits won |gp_n| binary | 1: win at a circuit; 0: lost at a cricuit|
+# MAGIC  |Race Participation|participation|binary| 1: participate in the season|
+# MAGIC  |Average Driver Point per race|avgpoints_c|numeral|per season per driver|
+# MAGIC  |The number of Engine Problems|engineproblem|numeral| 1 count for statusId = 5|
+# MAGIC  |The number of completed races|race_count|numeral| 1 count for a driver to have a position by the end of the race|
+# MAGIC  |Average Fastest Lap| avg_fastestlap|numeral| the lap number at which the team strategize for best performance|
+# MAGIC  |Average Fastest Speed| avg_fastestspeed|numeral|per season per driver|
+# MAGIC  |Number of unique drivers| unique_drivers|numeral|per season|
+# MAGIC  |Constructor ranking|position|numeral| by the end of the season|
 
 # COMMAND ----------
 
@@ -33,7 +36,7 @@ bucket = "group3-gr5069"
 
 # COMMAND ----------
 
-# Read data
+# Read constructor information data
 c = "processed/constructor_info.csv"
 
 obj = s3.get_object(Bucket= bucket, Key= c)
@@ -64,15 +67,20 @@ df_cc.head()
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC #### Feature I: Circuits
+# MAGIC #### Feature I: Circuits won
+# MAGIC 
+# MAGIC - binary 
+# MAGIC - 1: win at a circuit; 0: lost at a cricuit
 
 # COMMAND ----------
 
-df_1 = df_cc.merge(df[['raceId', 'circuitId']].drop_duplicates(), how = 'left', on = 'raceId')
+# append circuitId to race result
+df_1 = df_cc.merge(df[['raceId', 'circuitId']].drop_duplicates(), how = 'left', on = 'raceId') 
 df_1.head()
 
 # COMMAND ----------
 
+# pivot table so that the circuitIds become dummy variables. 
 df_11 = df_1.pivot_table(values=['wins'], index=['year', 'constructorId'], columns='circuitId')
 df_11.columns =['gp_' + str(s2) for (s1,s2) in df_11.columns.tolist()]
 df_11= df_11.fillna(0).reset_index()
@@ -91,7 +99,6 @@ df_x.head()'''
 
 # COMMAND ----------
 
-# average points in this season
 df_2 = df.groupby(['year','constructorId']).points.mean() # Average Point 
 df_2 = df_2.reset_index(name='avgpoints_c')
 df_2.loc[:, 'participation'] = 1 # Dummy variable for participation: 0 for absence, 1 for participation
@@ -99,6 +106,7 @@ df_2.head()
 
 # COMMAND ----------
 
+# merge with computed features
 df_x = df_2.merge(df_11, how = 'left', on = ['year', 'constructorId'])
 df_x.head()
 
@@ -131,6 +139,7 @@ df_3.head()
 
 # COMMAND ----------
 
+# merge with computed features
 df_x = df_3.merge(df_x, how = 'left', on = ['year', 'constructorId'])
 df_x.head()
 
@@ -140,6 +149,7 @@ df_x.head()
 
 # COMMAND ----------
 
+# count the number of race the drivers hired by each constructor finish with a position
 df_4 = df.groupby(['year', 'constructorId']).position.count()
 df_4 = df_4.reset_index(name='race_count')
 df_4 = df_4.fillna(0)
@@ -147,6 +157,7 @@ df_4.head()
 
 # COMMAND ----------
 
+# merge with computed features
 df_x = df_4.merge(df_x, how = 'left', on = ['year', 'constructorId'])
 df_x.head()
 
@@ -156,6 +167,7 @@ df_x.head()
 
 # COMMAND ----------
 
+# the average lap number the team strategize for best performance
 df_5 = df.groupby(['year', 'constructorId']).fastestLap.mean()
 df_5 = df_5.reset_index(name='avg_fastestlap')
 df_5 = df_5.fillna(0)
@@ -163,6 +175,7 @@ df_5.head()
 
 # COMMAND ----------
 
+# merge with computed features
 df_x = df_5.merge(df_x, how = 'left', on = ['year', 'constructorId'])
 df_x.head()
 
@@ -172,6 +185,7 @@ df_x.head()
 
 # COMMAND ----------
 
+# the average fastest spead the team achieved in the season
 df_6 = df.groupby(['year', 'constructorId']).fastestLapSpeed.mean()
 df_6 = df_6.reset_index(name='avg_fastestspeed')
 df_6 = df_6.fillna(0)
@@ -179,6 +193,7 @@ df_6.head()
 
 # COMMAND ----------
 
+# merge with computed features
 df_x = df_6.merge(df_x, how = 'left', on = ['year', 'constructorId'])
 df_x.head()
 
@@ -188,11 +203,13 @@ df_x.head()
 
 # COMMAND ----------
 
+# count the number of unique drivers each constructor hired for the season
 df_8 = df.groupby(['year', 'constructorId']).driverId.nunique().reset_index(name='unique_drivers')
 df_8.head()
 
 # COMMAND ----------
 
+# merge with computed features
 df_x = df_x.merge(df_8, how = 'left', on = ['year', 'constructorId'])
 df_x.head()
 
@@ -209,12 +226,14 @@ df_9.head()
 
 # COMMAND ----------
 
+# the constructor standing by the end of the last Grand Prix in each season
 df_9 = df_9[['year', 'constructorId', 'position']]
 df_x = df_x.merge(df_9, how = 'left', on = ['year', 'constructorId'])
 df_x.head()
 
 # COMMAND ----------
 
+# Compute some lag features: average driver points per race, participation, constructor ranking
 df_x['lag1_avg']=df_x.sort_values('year').groupby('constructorId')['avgpoints_c'].shift()
 df_x['lag2_avg']=df_x.sort_values('year').groupby('constructorId')['lag1_avg'].shift()
 df_x['lag1_ptc']=df_x.sort_values('year').groupby('constructorId')['participation'].shift()
