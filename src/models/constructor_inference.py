0a911aea1daf74cb5dcf917fbeae66098bf1db7a
Shen-dot-Xin <59472523+Shen-dot-Xin@users.noreply.github.com>
2021-04-12 23:33:23 -0400

    Update model logging

--- src/models/constructor_inference.py
+++ src/models/constructor_inference.py
@@ -19,22 +19,26 @@ df = spark.read.csv('s3://group3-gr5069/interim/constructor_features.csv', heade
 
 # COMMAND ----------
 
-window = Window.partitionBy('constructorId').orderBy(asc('year'))
+display(df)
 
 # COMMAND ----------
 
-df = df.withColumn("lag1_fs", lag("avg_fastestspeed", 1, 0).over(window))
-df = df.withColumn("lag2_fs", lag("avg_fastestspeed", 2, 0).over(window))
+# window = Window.partitionBy('constructorId').orderBy(asc('year'))
 
 # COMMAND ----------
 
-df = df.withColumn("lag1_fl", lag("avg_fastestlap", 1, 0).over(window))
-df = df.withColumn("lag2_fl", lag("avg_fastestlap", 2, 0).over(window))
+#df = df.withColumn("lag1_fs", lag("avg_fastestspeed", 1, 0).over(window))
+#df = df.withColumn("lag2_fs", lag("avg_fastestspeed", 2, 0).over(window))
 
 # COMMAND ----------
 
-df = df.withColumn("lag1_nd", lag("unique_drivers", 1, 0).over(window))
-df = df.withColumn("lag2_nd", lag("unique_drivers", 2, 0).over(window))
+#df = df.withColumn("lag1_fl", lag("avg_fastestlap", 1, 0).over(window))
+#df = df.withColumn("lag2_fl", lag("avg_fastestlap", 2, 0).over(window))
+
+# COMMAND ----------
+
+#df = df.withColumn("lag1_nd", lag("unique_drivers", 1, 0).over(window))
+#df = df.withColumn("lag2_nd", lag("unique_drivers", 2, 0).over(window))
 
 # COMMAND ----------
 
@@ -42,15 +46,17 @@ df.columns
 
 # COMMAND ----------
 
-cols_to_normalize = ['avg_fastestspeed',
- 'avg_fastestlap',
- 'race_count',
- 'engineproblem',
- 'avgpoints_c',  'unique_drivers',
- 'position',
- 'lag1_avg',
- 'lag2_avg', 'lag1_pst',
- 'lag2_pst']
+cols_to_normalize = ['avg_fastestspeed', 
+                     'avg_fastestlap',
+                     'race_count',
+                     'engineproblem',
+                     'avgpoints_c',  
+                     'unique_drivers',
+                     'position',
+                     'lag1_avg',
+                     'lag2_avg', 
+                     'lag1_pst',
+                     'lag2_pst']
 
 # COMMAND ----------
 
@@ -65,88 +71,32 @@ for c in cols_to_normalize:
 # COMMAND ----------
 
 feature_list =['avg_fastestspeed',
- 'avg_fastestlap',
- 'race_count',
- 'engineproblem',
- 'avgpoints_c',
- 'participation',
- 'gp_1',
- 'gp_2',
- 'gp_3',
- 'gp_4',
- 'gp_5',
- 'gp_6',
- 'gp_7',
- 'gp_8',
- 'gp_9',
- 'gp_10',
- 'gp_11',
- 'gp_12',
- 'gp_13',
- 'gp_14',
- 'gp_15',
- 'gp_16',
- 'gp_17',
- 'gp_18',
- 'gp_19',
- 'gp_20',
- 'gp_21',
- 'gp_22',
- 'gp_23',
- 'gp_24',
- 'gp_25',
- 'gp_26',
- 'gp_27',
- 'gp_28',
- 'gp_29',
- 'gp_30',
- 'gp_31',
- 'gp_32',
- 'gp_33',
- 'gp_34',
- 'gp_35',
- 'gp_36',
- 'gp_37',
- 'gp_38',
- 'gp_39',
- 'gp_40',
- 'gp_41',
- 'gp_42',
- 'gp_43',
- 'gp_44',
- 'gp_45',
- 'gp_46',
- 'gp_47',
- 'gp_48',
- 'gp_49',
- 'gp_50',
- 'gp_51',
- 'gp_52',
- 'gp_53',
- 'gp_54',
- 'gp_55',
- 'gp_56',
- 'gp_57',
- 'gp_58',
- 'gp_59',
- 'gp_60',
- 'gp_61',
- 'gp_62',
- 'gp_63',
- 'gp_64',
- 'gp_68',
- 'gp_69',
- 'gp_70',
- 'gp_71',
- 'gp_73',
- 'unique_drivers',
- 'position',
- 'lag1_avg',
- 'lag2_avg',
- 'lag1_ptc',
- 'lag2_ptc',
- 'lag1_pst',
- 'lag2_pst']
+               'avg_fastestlap',
+               'race_count',
+               'engineproblem',
+               'avgpoints_c',
+               'gp_1','gp_2','gp_3','gp_4','gp_5',
+               'gp_6','gp_7','gp_8','gp_9','gp_10',
+               'gp_11','gp_12','gp_13','gp_14','gp_15',
+               'gp_16','gp_17','gp_18','gp_19','gp_20',
+               'gp_21','gp_22','gp_23','gp_24','gp_25',
+               'gp_26','gp_27','gp_28','gp_29','gp_30',
+               'gp_31','gp_32','gp_33','gp_34','gp_35',
+               'gp_36','gp_37','gp_38','gp_39','gp_40',
+               'gp_41','gp_42','gp_43','gp_44','gp_45',
+               'gp_46','gp_47','gp_48','gp_49','gp_50',
+               'gp_51','gp_52','gp_53','gp_54','gp_55',
+               'gp_56','gp_57','gp_58','gp_59','gp_60',
+               'gp_61','gp_62','gp_63','gp_64','gp_68',
+               'gp_69','gp_70','gp_71','gp_73',
+               'unique_drivers',
+               'position',
+               'lag1_avg',
+               'lag2_avg',
+               'lag1_ptc',
+               'lag2_ptc',
+               'lag1_pst',
+               'lag2_pst']
 
 # COMMAND ----------
 
@@ -160,16 +110,6 @@ vecDF = vecAssembler.transform(df)
 
 # COMMAND ----------
 
-scalar = StandardScaler(inputCol="features", outputCol="ssFeatures")
-
-ssDF = scalar.fit(vecDF)
-
-# COMMAND ----------
-
-ssdf = ssDF.transform(vecDF)
-
-# COMMAND ----------
-
 # MAGIC %md 
 # MAGIC #### Logistic Regression
 
@@ -177,10 +117,112 @@ ssdf = ssDF.transform(vecDF)
 
 import os
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 import mlflow.sklearn
 import seaborn as sns
 import tempfile
 from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+
+# COMMAND ----------
+
+(trainDF, testDF) = vecDF.randomSplit([.8, .2], seed=42)
+
+# COMMAND ----------
+
+mlflow.sklearn.autolog()
+# With autolog() enabled, all model parameters, a model score, and the fitted model are automatically logged.  
+with mlflow.start_run():
+  lr = LogisticRegression(featuresCol ='features', labelCol = "champion")
+  lrModel = lr.fit(trainDF)
+
+  predictions = lrModel.transform(testDF)
+  
+  evaluator= BinaryClassificationEvaluator(labelCol='champion')
+  
+  trainingSummary = lrModel.summary
+  # Create metrics
+  #objectiveHistory = trainingSummary.objectiveHistory
+  accuracy = trainingSummary.accuracy
+  precision = trainingSummary.weightedPrecision
+  recall = trainingSummary.weightedRecall
+  
+  falsePositiveRate = trainingSummary.weightedFalsePositiveRate
+  truePositiveRate = trainingSummary.weightedTruePositiveRate
+  
+  #fMeasure = trainingSummary.weightedFMeasure()
+  areaUnderROC = trainingSummary.areaUnderROC
+  testAreaUnderROC = evaluator.evaluate(predictions)
+  
+  # Log metrics
+  #mlflow.log_metric("objectiveHistory", objectiveHistory)
+  mlflow.log_metric("accuracy", accuracy)
+  mlflow.log_metric("falsePositiveRate", falsePositiveRate)
+  mlflow.log_metric("truePositiveRate", truePositiveRate)
+  #mlflow.log_metric("fMeasure", fMeasure)
+  mlflow.log_metric("precision", precision)
+  mlflow.log_metric("recall", recall)
+  mlflow.log_metric("areaUnderROC", areaUnderROC)
+  mlflow.log_metric("testAreaUnderROC", testAreaUnderROC)
+  # Collecting the Feature Importance through Model Coefficients
+  importance = pd.DataFrame(list(zip(trainDF.columns, lrModel.coefficients)), 
+                            columns=["Feature", "Importance"]
+                          ).sort_values("Importance", ascending=False)
+  
+  # Log importances using a temporary file
+  temp = tempfile.NamedTemporaryFile(prefix="feature-importance-", suffix=".csv")
+  temp_name = temp.name
+  try:
+    importance.to_csv(temp_name, index=False)
+    mlflow.log_artifact(temp_name, "feature-importance.csv")
+  finally:
+    temp.close() # Delete the temp file
+  #Create ROC plot
+  roc = trainingSummary.roc.toPandas()
+  plt.plot(roc['FPR'],roc['TPR'])
+  plt.ylabel('False Positive Rate')
+  plt.xlabel('True Positive Rate')
+  plt.title('ROC Curve')
+  # Log ROC plot using a temporary file
+  temp = tempfile.NamedTemporaryFile(prefix="ROC-Curve", suffix=".png")
+  temp_name = temp.name
+  try:
+    plt.savefig(temp_name)
+    mlflow.log_artifact(temp_name, "ROC-Curve.png")
+  finally:
+    temp.close() # Delete the temp file
+  plt.show()
+  #Create Beta-Coeff plot
+  beta = np.sort(lrModel.coefficients)
+  plt.plot(beta)
+  plt.ylabel('Coefficients')
+  plt.title('Coefficients Graph')
+  # Log Beta-Coeff plot using a temporary file
+  temp = tempfile.NamedTemporaryFile(prefix="Coeff", suffix=".png")
+  temp_name = temp.name
+  try:
+    plt.savefig(temp_name)
+    mlflow.log_artifact(temp_name, "Coeff.png")
+  finally:
+    temp.close() # Delete the temp file
+  plt.show()
+  #Create Precision-Recall plot
+  pr = trainingSummary.pr.toPandas()
+  plt.plot(pr['recall'],pr['precision'])
+  plt.ylabel('Precision')
+  plt.xlabel('Recall')
+  plt.title('Precision-Recall Curve')
+  # Log Precision-Recall plot using a temporary file
+  temp = tempfile.NamedTemporaryFile(prefix="Precision-Recall", suffix=".png")
+  temp_name = temp.name
+  try:
+    plt.savefig(temp_name)
+    mlflow.log_artifact(temp_name, "Precision-Recall.png")
+  finally:
+    temp.close() # Delete the temp file
+  plt.show()
+  print('Training set areaUnderROC: ' + str(trainingSummary.areaUnderROC))
 
 # COMMAND ----------
 
@@ -188,11 +230,7 @@ df.count()
 
 # COMMAND ----------
 
-(trainDF, testDF) = ssdf.randomSplit([.8, .2], seed=42)
-
-# COMMAND ----------
-
-lr = LogisticRegression(featuresCol = "ssFeatures", labelCol = "champion")
+lr = LogisticRegression(featuresCol = "features", labelCol = "champion")
 lrModel = lr.fit(trainDF)
 
 # COMMAND ----------
@@ -211,10 +249,6 @@ print('Training set areaUnderROC: ' + str(trainingSummary.areaUnderROC))
 
 # COMMAND ----------
 
-['race_count','engineproblem','unique_drivers','lag1_avg','lag2_avg','lag1_fs','lag2_fs','lag1_fl','lag2_fl','lag1_nd','lag2_nd','lag1_standing','lag2_standing']
-
-# COMMAND ----------
-
 lrModel.coefficients
 
 # COMMAND ----------
@@ -224,6 +258,15 @@ plt.plot(pr['recall'],pr['precision'])
 plt.ylabel('Precision')
 plt.xlabel('Recall')
 plt.show()
+
+# COMMAND ----------
+
+predDF = lrModel.transform(testDF)
+display(predDF)
+
+# COMMAND ----------
+
+
 
 # COMMAND ----------
 
@@ -326,11 +369,6 @@ log_lr("test Run", features = feature_list)
 
 # COMMAND ----------
 
-lr = LinearRegression(featuresCol = "features", labelCol = "champion")
-lrModel = lr.fit(vecDF)
-
-# COMMAND ----------
-
 
 
 # COMMAND ----------
@@ -417,10 +455,6 @@ def log_lr(experimentID, run_name, features):
 
 # COMMAND ----------
 
-(trainDF, testDF) = df.randomSplit([.8, .2], seed=42)
-
-# COMMAND ----------
-
 print(trainDF.cache().count())
 
 # COMMAND ----------
@@ -439,14 +473,104 @@ print(lrModel.summary.r2)
 
 # COMMAND ----------
 
-predDF_final = predDF.select('host_is_superhost','cancellation_policy','instant_bookable','host_total_listings_count','neighbourhood_cleansed','zipcode','latitude','longitude','property_type','room_type','accommodates','bathrooms','bedrooms','beds','bed_type','minimum_nights','number_of_reviews','review_scores_rating','review_scores_accuracy','review_scores_cleanliness','review_scores_checkin','review_scores_communication','review_scores_location','review_scores_value','price','bedrooms_na','bathrooms_na','beds_na','review_scores_rating_na','review_scores_accuracy_na','review_scores_cleanliness_na','review_scores_checkin_na','review_scores_communication_na','review_scores_location_na','review_scores_value_na','prediction')
+predDF.columns
+
+# COMMAND ----------
+
+predDF_final = predDF.select('year',
+ 'constructorId',
+ 'avg_fastestspeed',
+ 'avg_fastestlap',
+ 'race_count',
+ 'engineproblem',
+ 'avgpoints_c',
+ 'participation',
+ 'gp_1',
+ 'gp_2',
+ 'gp_3',
+ 'gp_4',
+ 'gp_5',
+ 'gp_6',
+ 'gp_7',
+ 'gp_8',
+ 'gp_9',
+ 'gp_10',
+ 'gp_11',
+ 'gp_12',
+ 'gp_13',
+ 'gp_14',
+ 'gp_15',
+ 'gp_16',
+ 'gp_17',
+ 'gp_18',
+ 'gp_19',
+ 'gp_20',
+ 'gp_21',
+ 'gp_22',
+ 'gp_23',
+ 'gp_24',
+ 'gp_25',
+ 'gp_26',
+ 'gp_27',
+ 'gp_28',
+ 'gp_29',
+ 'gp_30',
+ 'gp_31',
+ 'gp_32',
+ 'gp_33',
+ 'gp_34',
+ 'gp_35',
+ 'gp_36',
+ 'gp_37',
+ 'gp_38',
+ 'gp_39',
+ 'gp_40',
+ 'gp_41',
+ 'gp_42',
+ 'gp_43',
+ 'gp_44',
+ 'gp_45',
+ 'gp_46',
+ 'gp_47',
+ 'gp_48',
+ 'gp_49',
+ 'gp_50',
+ 'gp_51',
+ 'gp_52',
+ 'gp_53',
+ 'gp_54',
+ 'gp_55',
+ 'gp_56',
+ 'gp_57',
+ 'gp_58',
+ 'gp_59',
+ 'gp_60',
+ 'gp_61',
+ 'gp_62',
+ 'gp_63',
+ 'gp_64',
+ 'gp_68',
+ 'gp_69',
+ 'gp_70',
+ 'gp_71',
+ 'gp_73',
+ 'unique_drivers',
+ 'position',
+ 'lag1_avg',
+ 'lag2_avg',
+ 'lag1_ptc',
+ 'lag2_ptc',
+ 'lag1_pst',
+ 'lag2_pst',
+ 'champion',
+ 'prediction')
 
 # COMMAND ----------
 
 predDF_final.write.format('jdbc').options(
       url='jdbc:mysql://sx2200-gr5069.ccqalx6jsr2n.us-east-1.rds.amazonaws.com/sx2200',
       driver='com.mysql.jdbc.Driver',
-      dbtable='test_airbnb_preds',
+      dbtable='test_lr_preds',
       user='admin',
       password='Xs19980312!').mode('overwrite').save()
 
